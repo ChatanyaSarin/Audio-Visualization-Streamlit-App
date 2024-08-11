2bf5006c4b588937fe69e8ee203592f9b3939153
ChatanyaSarin <78634988+ChatanyaSarin@users.noreply.github.com>
2024-08-10 19:17:33 -0500

    Add files via upload

--- Audio_Visualization_App.py
+++ Audio_Visualization_App.py
@@ -4,6 +4,7 @@ import pandas as pd
 import wave
 import matplotlib.pyplot as plt
 import plotly.express as px
+import librosa
 
 #HELPER FUNCTIONS
 #Displays a particular emotion
@@ -21,11 +22,14 @@ def readAudio (inputFile):
     file = wave.open(inputFile, 'rb') #opens the file using wave
     frequency = file.getframerate()
     nsamples = file.getnframes()
+    nchannels = file.getnchannels()
     audioTime = nsamples/frequency
     signal = file.readframes(nsamples)
     signalArray = np.frombuffer(signal, dtype = np.int16)
     signalArray = signalArray.reshape(-1)
     times = np.linspace(0, audioTime, num = nsamples)
+    if nchannels > 1:
+        signalArray = signalArray[:int(len(signalArray)/nchannels)]
 
     return times, signalArray
 
@@ -70,10 +74,10 @@ def plot3dSpectogram (times, signal):
 #Plots amplitude using signal and plotly
 @st.cache_data
 def plotAmplitudeFFT (time, signal):
-    amplitudeFFT = np.fft.fft(signal)
+    amplitudeFFT = np.abs(np.fft.fft(signal))
     fourierDf = pd.DataFrame({
         "Times": time,
-        "Amplitude": np.abs(amplitudeFFT),
+        "Amplitude": amplitudeFFT,
     })
     return(px.line(fourierDf, x = "Times", y = "Amplitude", log_x = True))
 
@@ -88,7 +92,7 @@ if inputFile is not None:
     st.audio(inputFile) #Creates audio player
     times, signalArray = readAudio(inputFile)
     cacheAllData(inputFile, times, signalArray) 
-    #displayEmotion("sadness")
+    displayEmotion("sadness")
 
     buttonOptions = ["Frequency", "Spectogram", "Amplitude", "3D"] #Creates radio buttons for different graphs
 
